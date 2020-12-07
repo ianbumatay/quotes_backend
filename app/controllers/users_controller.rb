751a44a4f77a3ab11efdb883d57a27f7ac8b63c0
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2020-12-07 09:12:05 -0500

    build full CRUD for user model class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,44 @@ class UsersController < ApplicationController
 
       render json: users, only: [:username]
       #only: [:id, :name, :species]
+    end 
+
+    def show 
+      user = User.find(params[:id])
+      render json: user
+    end 
+
+    def create 
+      user = User.new(user_params) 
+
+      if user.save 
+        render json: user, status: :created, location: :user 
+      else 
+        render json: user.errors, status: :unprocessable_entity 
+      end
+    end  
+
+    def update 
+      user = User.find(params[:id]) 
+
+      if user.update(user_params)
+        render json: user 
+      else 
+        render json: user.errors, status: :unprocessable_entity  
+      end 
+    end 
+
+    def destroy 
+      user = User.find(params[:id])
+
+      user.destroy
     end
+
+
+    private 
+
+    def user_params 
+      params.require(user).permit(:id, :username, :email, :password)
+    end
+
 end
